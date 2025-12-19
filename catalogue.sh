c276247e1da0dcda4b14cab927633d3e5b496f9e
srikanth152113 <srikanth152113@gmail.com>
2025-12-19 17:06:41 +0530

    dry code

--- catalogue.sh
+++ catalogue.sh
@@ -1,11 +1,10 @@
 component_name=catalogue
 source common.sh
 pwd=${pwd}
+
 nodejs
 
 cp ${pwd}/mongo.repo /etc/yum.repos.d/mongo.repo
-
-
 
 dnf install mongodb-mongosh -y
 mongosh --host mongodb-dev.srikanth553.store </app/db/master-data.js
--- common.sh
+++ common.sh
@@ -20,3 +20,17 @@ nodejs(){
     npm install
 }
 pwd=${pwd}
+
+python(){
+    dnf install python3 gcc python3-devel -y
+    cp {component_name}.service /etc/systemd/system/{component_name}.service
+    useradd roboshop
+
+    mkdir /app
+    curl -L -o /tmp/{component_name}.zip https://roboshop-artifacts.s3.amazonaws.com/{component_name}-v3.zip 
+    cd /app 
+    unzip /tmp/{component_name}.zip
+
+    cd /app 
+    pip3 install -r requirements.txt
+}
