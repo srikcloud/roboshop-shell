df46b7d2efcf0c4bdfb0c5639b67a1cbe848232a
srikanth152113 <srikanth152113@gmail.com>
2025-12-19 16:53:42 +0530

    dry code

--- catalogue.sh
+++ catalogue.sh
@@ -1,10 +1,11 @@
 component_name=catalogue
 source common.sh
-
-cp catalogue.service /etc/systemd/system/catalogue.service
-cp mongo.repo /etc/yum.repos.d/mongo.repo
-
+pwd=${pwd}
 nodejs
+
+cp ${pwd}/mongo.repo /etc/yum.repos.d/mongo.repo
+
+
 
 dnf install mongodb-mongosh -y
 mongosh --host mongodb-dev.srikanth553.store </app/db/master-data.js
