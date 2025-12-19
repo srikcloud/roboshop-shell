80d93675369d5fa62ab002affe0035759b44e131
srikanth152113 <srikanth152113@gmail.com>
2025-12-19 17:17:32 +0530

    dry code

--- common.sh
+++ common.sh
@@ -1,7 +1,20 @@
+pwd=${pwd}
 systemd_setup(){
+    cp ${pwd}/${component_name}.service /etc/systemd/system/${component_name}.service
     systemctl daemon-reload
     systemctl enable ${component_name} 
     systemctl start ${component_name}
+}
+
+app_pre_setup(){
+    useradd roboshop
+    rm -rf /app
+    mkdir /app
+    cd /app
+    npm install
+    curl -L -o /tmp/${component_name}.zip https://roboshop-artifacts.s3.amazonaws.com/${component_name}-v3.zip 
+    unzip /tmp/${component_name}.zip
+    
 }
 
 nodejs(){
@@ -9,28 +22,12 @@ nodejs(){
     dnf module disable nodejs -y
     dnf module enable nodejs:20 -y
     dnf install nodejs -y
-    useradd roboshop
-    rm -rf /app
-    mkdir /app
-    cd /app 
-    npm install
-    curl -L -o /tmp/${component_name}.zip https://roboshop-artifacts.s3.amazonaws.com/cart-v3.zip
-    cd /app 
-    unzip /tmp/${component_name}.zip
+    app_pre_setup
     npm install
 }
-pwd=${pwd}
 
 python(){
     dnf install python3 gcc python3-devel -y
-    cp {component_name}.service /etc/systemd/system/{component_name}.service
-    useradd roboshop
-
-    mkdir /app
-    curl -L -o /tmp/{component_name}.zip https://roboshop-artifacts.s3.amazonaws.com/{component_name}-v3.zip 
-    cd /app 
-    unzip /tmp/{component_name}.zip
-
-    cd /app 
+    app_pre_setup
     pip3 install -r requirements.txt
 }
