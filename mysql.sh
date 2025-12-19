244581621fdb9ba5479039bf54a10333f87dce16
srikanth152113 <srikanth152113@gmail.com>
2025-12-19 23:38:41 +0530

    dry code

--- mysql.sh
+++ mysql.sh
@@ -3,17 +3,18 @@ echo Input password is missing
 exit
 fi
 
-#if [ -n "${MYSQL_ROOT_PASSWORD}" ]; then
-#  PASSWORD=$MYSQL_ROOT_PASSWORD
-#fi
+if [ -n "${MYSQL_ROOT_PASSWORD}" ]; then
+  PASSWORD=$MYSQL_ROOT_PASSWORD
+fi
 
-#if [ -n "$1" ];then
- # PASSWORD=$1
-#fi
+if [ -n "$1" ];then
+ PASSWORD=$1
+fi
 
 dnf install mysql-server -y
 
 systemctl enable mysqld
 systemctl start mysqld 
 
-mysql_secure_installation --set-root-pass $MYSQL_ROOT_PASSWORD
+mysql_secure_installation --set-root-pass $PASSWORD
+
