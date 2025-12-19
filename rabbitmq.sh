2480c1b929f452793189e606d4d0642a4f257a7e
srikanth152113 <srikanth152113@gmail.com>
2025-12-19 23:48:11 +0530

    updated

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -8,5 +8,5 @@ dnf install rabbitmq-server -y
 
 systemctl enable rabbitmq-server
 systemctl start rabbitmq-server
-rabbitmqctl add_user roboshop roboshop $1
+rabbitmqctl add_user roboshop $1
 rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
