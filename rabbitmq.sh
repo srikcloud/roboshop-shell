d5f39dd12c97192128f0e69a2cb6636856859c12
srikanth152113 <srikanth152113@gmail.com>
2025-12-19 23:49:52 +0530

    updated

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -1,6 +1,6 @@
-if [-z "$1" ]; then
- echo Input password is missing
- exit
+if [ -z "$1" ]; then
+  echo Input password is missing
+  exit 1
 fi
 
 cp rabbitmq.repo /etc/yum.repos.d/rabbitmq.repo
@@ -8,5 +8,6 @@ dnf install rabbitmq-server -y
 
 systemctl enable rabbitmq-server
 systemctl start rabbitmq-server
+
 rabbitmqctl add_user roboshop $1
 rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
